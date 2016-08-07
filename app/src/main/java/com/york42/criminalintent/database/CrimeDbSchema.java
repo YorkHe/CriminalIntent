9c6df3e538d718dbdf8a0a7c2b86fad8bc566a37
York <kiddmagician@gmail.com>
2016-08-07 21:38:27 +0800

    Initial and final commit

---
+++ app/src/main/java/com/york42/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,18 @@
+package com.york42.criminalintent.database;
+
+/**
+ * Created by 宇 on 2016/8/7.
+ */
+public class CrimeDbSchema {
+    public static final class CrimeTable{
+        public static final String NAME = "crimes";
+
+        public static final class Cols{
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+            public static final String SUSPECT = "suspect";
+        }
+    }
+}
